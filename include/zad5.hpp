563edfbd153e3c47b6b0eb0ecfdf962f545c5d40
KamilStaronski <73498649+KamilStaronski@users.noreply.github.com>
2021-01-25 19:12:50 +0100

    Update zad5.hpp

--- include/zad5.hpp
+++ include/zad5.hpp
@@ -5,3 +5,7 @@
 #include <vector>
 
 // tutaj algorytm liczKolejneWarzywa
+template <typename Iterator>
+bool liczKolejnewarzywa(Iterator begin, Iterator end, unsigned n, const std::string s) {
+   //return std::search_n(begin, end, )
+}
