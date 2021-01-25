551688825c00e09eb3e8c5ecedd75497be39271b
KamilStaronski <73498649+KamilStaronski@users.noreply.github.com>
2021-01-25 18:58:07 +0100

    Update zad4.hpp

--- include/zad4.hpp
+++ include/zad4.hpp
@@ -5,3 +5,11 @@
 #include <vector>
 
 // tutaj funkcja jedzOstatnieWarzywa
+void jedzOstatnieWarzywa(const std::vector<Warzywo> &wek, unsigned n, std::ostream& os) {
+    if (n <= wek.size()) {
+       throw std::logic error{"error};
+    }
+        for (int i = wek.size() - 1; i >= wek.size() - n - 1; i--) {
+            os[i].opis(wek);
+        }
+}
