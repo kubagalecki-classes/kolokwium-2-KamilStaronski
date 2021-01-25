151bd182a9d8145efe71f4590b2df4c9f9832e50
KamilStaronski <73498649+KamilStaronski@users.noreply.github.com>
2021-01-25 18:03:52 +0100

    Update zad3.hpp

--- include/zad3.hpp
+++ include/zad3.hpp
@@ -7,6 +7,6 @@
 // tutaj szablon gotujZupe
 template <typename T>
 unsigned gotujZupe(const Warzywo& w, const T& obiekt){
-    unsigned wynik = obiekt.gotuj(w);
+    unsigned wynik = obiekt.gotujZupe(w);
     return wynik*wynik;
 }
