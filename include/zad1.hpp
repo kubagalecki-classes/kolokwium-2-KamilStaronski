8f0ca513499c1c60f0de681d544954a84fc9195e
KamilStaronski <73498649+KamilStaronski@users.noreply.github.com>
2021-01-25 18:25:50 +0100

    Update zad1.hpp

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -22,7 +22,7 @@ public:
         arg << nazwa << ": " << cena << ", " <<  kolor << std::endl;
     }
 
-    static unsigned getVeg(){return liczba_warzyw;}
+    inline static unsigned getVeg(){return liczba_warzyw;}
 };
 
 inline unsigned Warzywo::liczba_warzyw = 0;
